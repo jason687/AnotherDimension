b946cf730abf2bc41b5f493584abe3a03b7dbaaa
Jason Zhang <jzhang20@stuy.edu>
2020-10-24 11:55:01 -0400

    largest done

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,18 @@
 public class ArrayOps {
+
   public static int sum (int [] arr) {
     int add = 0;
     for (int i = 0; i < arr.length; i++) {
       add = arr[i] + add;
     }
     return add;
+  }
+
+  public static int largest (int [] arr) {
+    double temp = Double.NEGATIVE_INFINITY;
+    for (int i = 0; i < arr.length; i++) {
+      temp = Math.max(temp, arr[i]);
+    }
+    return ((int) temp);
   }
 }
--- Tester.java
+++ Tester.java
@@ -2,5 +2,6 @@ public class Tester {
   public static void main (String[] args) {
     int [] arr = {1, 2, 3, 4};
     System.out.println(ArrayOps.sum(arr));
+    System.out.println(ArrayOps.largest(arr));
   }
 }
